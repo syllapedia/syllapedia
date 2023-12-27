773586f35ad1d8bbd6279ac767441c686f03cc93
attilapalabiyik <123512351+attilapalabiyik@users.noreply.github.com>
2023-12-26 19:28:35 -0500

    Test Gemini Accuracy and Edge Cases
    * Accuracy tested here https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0Fdjg45Mn8B0b238TJZUNAHRZAFNuOlUs4w4rRA6DhYLIMiQnMJv4-8_aYRYnl_mx8v1otJETSz67/pubhtml
    * Prompt changed to account for simple answers and lack of role

---
+++ api/gemini_api.py
@@ -0,0 +1,35 @@
+import google.generativeai as genai
+from database import db
+import json
+
+courses = db["Courses"]
+
+def gemini_chat_respond(question, txt):
+    prompt_parts = [
+    """
+    You are an academic assistant, specialized in addressing questions specifically related to the current course. 
+    Your responses are tailored to the course content and syllabus, and you provide sources from the course material. 
+    You focus solely on assisting with course-related queries.
+
+    Using [%s], answer [%s].
+    
+    Format your response as a json in this format {"answer": string, "sources": string[]}.
+    
+    "answer" is your answer to the question. It should be clear and helpful. It should only get information from the syllabus.
+    
+    "sources" is a list of strings that contains only characters that are direct quotes from the syllabus that support the answer.
+    
+    Split source strings if there is a new line or if there are any characters that are bulleted or in a list.
+    
+    """ % (txt, question)
+    ]
+    try:
+        genai.configure(api_key="AIzaSyCbU8m8uacogSPwrcITcZEtw5ILxEA7v6w")
+        model = genai.GenerativeModel(model_name="gemini-pro")
+        content = model.generate_content(prompt_parts)
+        response = json.loads(content.text)
+        answer = response["answer"]
+        sources = response["sources"]
+        return {"answer": answer, "sources": sources, "valid": True}
+    except:
+        return {"answer": "Response Failed to Complete", "sources": "Response Failed to Complete", "valid": False}
