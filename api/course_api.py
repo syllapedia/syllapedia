e8136d689235ebeed9373f4a79d7085a27608ecc
algispetlin <apetlin@umass.edu>
2024-01-05 22:43:32 -0500

    Use JWT from Google to Authenticate API Requests
    1) Added authorization function to each API request in order to add oauth protection to API
    2) Created .env file to store Gemini API Key
    3) Added authToken to headers of each request in httpService
    4) Store jwt credential in userInfoSlice for use throughout application
    5) Added an error message on the login page for when a request to the Syllapedia API fails

--- api/course_api.py
+++ api/course_api.py
@@ -1,27 +1,40 @@
 from flask import request
 from apiflask import APIBlueprint
 from course_functions import set_course, new_course, get_course_data, delete_course_data
+from authorization import authorize
 
 course_api = APIBlueprint("course_api", __name__)
 
 @course_api.route("/course", methods=["POST"])
 def create_course():
-    data = request.get_json()
-    # Creates a course and returns the created course
-    return new_course(data["subject"], data["course_number"], data["course_title"], data["user_id"], data["syllabus"])
+    def action():
+        data = request.get_json()
+        # Creates a course and returns the created course
+        return new_course(data["subject"], data["course_number"], data["course_title"], data["user_id"], data["syllabus"])
+    
+    return authorize(action)
 
 @course_api.route("/course/<course_id>", methods=["GET"])
 def get_course(course_id):
-    # Returns a course
-    return get_course_data(course_id)
+    def action():
+        # Returns a course
+        return get_course_data(course_id)
+    
+    return authorize(action)
 
 @course_api.route("/course/<course_id>", methods=["PATCH"])
 def update_course(course_id):
-    data = request.get_json()
-    # Updates a value in a course
-    return set_course(course_id, data["key"], data["value"])
+    def action():
+        data = request.get_json()
+        # Updates a value in a course
+        return set_course(course_id, data["key"], data["value"])
+    
+    return authorize(action)
 
 @course_api.route("/course/<course_id>", methods=["DELETE"])
 def delete_course(course_id):
-    # Deletes a course
-    return delete_course_data(course_id)
+    def action():
+        # Deletes a course 
+        return delete_course_data(course_id)
+
+    return authorize(action)
