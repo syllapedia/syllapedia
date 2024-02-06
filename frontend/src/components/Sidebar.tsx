e8b6e78005704ccdf0732190de3d8e9c9e7dc09e
attilapalabiyik <123512351+attilapalabiyik@users.noreply.github.com>
2024-02-06 18:38:18 -0500

    Fix Environmental Variables in API
    Now the API uses os for getting env keys

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -48,7 +48,7 @@ function Sidebar() {
     };
 
     return (
-        <div className="root">
+        <div className="drawer">
             <CreateDialog open={dialogOpen} handleDialog={handleDialog}/>
 
             <Drawer
