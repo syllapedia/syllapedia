328085039e04e0cd0e667d7258b3e97c7df0ef3f
algispetlin <apetlin@umass.edu>
2024-02-17 17:54:15 -0500

    Add Chat Page Functionality
    1) Added cancel to chat requests
    2) Allow user to refresh the response
    3) When the selected course changes, refresh the page
    4) Added a button to view the original syllabus
    5) Fixed loading bubble

--- frontend/src/components/ChatInput.tsx
+++ frontend/src/components/ChatInput.tsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './ChatInput.css';
 import { useAppSelector, useAppDispatch } from "../app/hooks";
 import { selectChatbotState, updateQuestion, processQuestion, resetChatbot } from "../features/chatbot/chatbotSlice";
 import SendIcon from '@mui/icons-material/Send';
 import ReplayIcon from '@mui/icons-material/Replay';
+import LoopIcon from '@mui/icons-material/Loop';
+import DoDisturbIcon from '@mui/icons-material/DoDisturb';
 import { IconButton, Input, useTheme } from '@mui/material';
+import { abortHttpRequests } from "../services/httpService";
 
 function ChatInput() {
-    const chatbot = useAppSelector(selectChatbotState);
+    const chatbotState = useAppSelector(selectChatbotState);
     const dispatch = useAppDispatch();
 
     const theme = useTheme()
@@ -16,8 +19,8 @@ function ChatInput() {
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => setMessage(event.target.value);
 
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
+    const handleSubmit = async (event?: React.FormEvent) => {
+        if (event) event.preventDefault();
         setMessage("");
         
         if (message.length !== 0) {
@@ -28,22 +31,47 @@ function ChatInput() {
     }
 
     const handleReset = () => {
-        setIsEnabled(true);
+        setIsEnabled(chatbotState.course ? true : false);
         dispatch(resetChatbot());
     }
+
+    const handleRegen = () => {
+        const question = chatbotState.question;
+
+        dispatch(resetChatbot());
+        dispatch(updateQuestion(question));
+        dispatch(processQuestion());
+    }
+
+    const hexToRgba = (hex: string, opacity: number): string => {
+        hex = hex.replace('#', '');
+    
+        const r = parseInt(hex.substring(0, 2), 16);
+        const g = parseInt(hex.substring(2, 4), 16);
+        const b = parseInt(hex.substring(4, 6), 16);
+    
+        return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+    }
+
+    useEffect(() => handleReset(), [chatbotState.course]);
 
     return (
         <>
             {
-                isEnabled || chatbot.status === "loading" ? 
+                !chatbotState.course || isEnabled ? 
                     <div style={{ height: 25 }}></div> 
+                : chatbotState.status === "loading" ?
+                    <div className="reset-button" onClick={abortHttpRequests}>
+                        <DoDisturbIcon style={{ paddingRight: 5 }} />
+                        <div style={{ marginBottom: 3.5 }}>Cancel</div>
+                    </div>
                 :
-                    <div className="reset-button" onClick={handleReset}>
-                        <ReplayIcon style={{ paddingRight: 5 }} />
-                        <div style={{ marginBottom: 3.5 }}>Reset</div>
+                    <div className="reset-button" onClick={handleRegen}>
+                        <LoopIcon style={{ paddingRight: 5 }} />
+                        <div style={{ marginBottom: 3.5 }}>Regenerate</div>
                     </div>
             }
-            <form className="input-container" style={{ opacity: isEnabled ? 1 : 0.3, borderColor: theme.palette.primary.main }} onSubmit={handleSubmit}>
+            <form className="input-container" style={{ borderColor: hexToRgba(theme.palette.primary.main, isEnabled ? 1 : 0.3) }} onSubmit={handleSubmit}>
                 <Input 
                     type="text"
                     className="text-input" 
@@ -52,16 +80,35 @@ function ChatInput() {
                     value={message}
                     disabled={!isEnabled}
                     disableUnderline
+                    style={{ opacity: isEnabled ? 1 : 0.3 }}
                 />
-                <IconButton type="submit" disableRipple disabled={!isEnabled}>
-                    <SendIcon 
-                        style={{ 
-                            backgroundColor: theme.palette.primary.main,
-                            padding: "7px",
-                            color: "#e5e5e5",
-                            borderRadius: "100%" 
-                        }} 
-                    />
+                <IconButton 
+                    type="submit" 
+                    disableRipple 
+                    disabled={chatbotState.status === "loading"} 
+                    onClick={!isEnabled && chatbotState.status !== "loading" ? handleReset : () => {}} 
+                >
+                    {
+                        isEnabled || chatbotState.status === "loading" ?
+                            <SendIcon 
+                                style={{
+                                    opacity: isEnabled ? 1 : 0.3, 
+                                    backgroundColor: theme.palette.primary.main,
+                                    padding: "7px",
+                                    color: "#e5e5e5",
+                                    borderRadius: "100%" 
+                                }} 
+                            />
+                        :    
+                            <ReplayIcon 
+                                style={{ 
+                                    backgroundColor: theme.palette.primary.main,
+                                    padding: "7px",
+                                    color: "#e5e5e5",
+                                    borderRadius: "100%" 
+                                }} 
+                            />
+                    }
                 </IconButton>
             </form>
             <div className="disclaimer">Note: Syllapedia may make mistakes when answering questions or highlighting sources</div>
